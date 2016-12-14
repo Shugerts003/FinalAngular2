718f005c78b3a754865c9843d31a8542e8c840aa
Shugerts <shugerts003@gannon.edu>
2016-12-13 19:55:43 -0500

    02_04

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,5 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: './app.Component.html'
 })
 export class AppComponent {
+  name: string;
+  artists: any;
 
+  constructor() {
+    this.name =  'Ray';
+    this.artists = [
+      {
+          name: 'Barot Bellingham',
+          school: 'Penn State'
+      },{
+          name: 'Hillary Post',
+          school: 'University of Illinois'
+      }
+    ];
+  }
 }
